c3ab4885144cf10d3e06f0eef7c5277e493e43f2
EakenderRana <buntychauhan010000@github.com>
2025-07-11 11:46:57 +0530

    hello bro

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import { useEffect, useState } from "react";
+import Aos from "aos";
+import "aos/dist/aos.css";
+
+// Components
+import Hero from "./components/Hero";
 import InfoCards from "./components/InfoCards";
 import Features from "./components/Features";
-import Hero from "./components/Hero";
 import LogoSliders from "./components/LogoSliders";
 import Corporis from "./components/Corporis";
 import UnleashingPotential from "./components/UnleashingPotential";
@@ -10,29 +15,35 @@ import Enimquis from "./components/Enimquis";
 import Pricing from "./components/Pricing";
 import Frequently from "./components/Frequently";
 import Testimonials from "./components/Testimonials";
+import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
-import "aos/dist/aos.css";
-import Contact from "./components/Contact";
-import Aos from "aos";
-import { useEffect } from "react";
-
-
-  
-
 function App() {
-useEffect(() => {
-  Aos.init({
-    duration: 1000, // Animation duration (in milliseconds)
-    once: true, // Only animate once when scrolling down
-  });
-}, []);
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    Aos.init({ duration: 1000, once: true });
+
+    // Simulate loading time
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1500); // Adjust this time as needed
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="w-screen h-screen flex justify-center items-center bg-white">
+        <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-black-500 border-solid"></div>
+      </div>
+    );
+  }
 
   return (
     <>
       <Hero />
-      <InfoCards/>
+      <InfoCards />
       <UnleashingPotential />
       <LogoSliders />
       <Features />
@@ -44,8 +55,7 @@ useEffect(() => {
       <Frequently />
       <Testimonials />
       <Contact />
-      <Footer/>
-      
+      <Footer />
     </>
   );
 }
