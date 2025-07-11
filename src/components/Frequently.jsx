cccc74855296a985858a2d37b044a39c1d16cee2
EakenderRana <buntychauhan010000@github.com>
2025-07-11 20:49:53 +0530

    New Verson

--- src/components/Frequently.jsx
+++ src/components/Frequently.jsx
@@ -14,13 +14,21 @@ function Frequently() {
       {/* Section Title */}
       <h2
         data-aos="fade-up"
-        data-aos-delay="100"
+        
         className="text-3xl font-bold text-center text-secondary"
       >
         Frequently Asked Questions
       </h2>
-      <div className="w-12 h-1 bg-primary mx-auto my-3" />
-      <p className="text-center text-secondary mb-10">
+      <div
+        data-aos="fade-up"
+        data-aos-delay="10"
+        className="w-12 h-1 bg-primary mx-auto my-3"
+      />
+      <p
+        data-aos="fade-up"
+        data-aos-delay="20"
+        className="text-center text-secondary mb-10"
+      >
         Necessitatibus eius consequatur ex aliquid fuga eum quidem sint
         consectetur velit
       </p>
@@ -32,8 +40,6 @@ function Frequently() {
           return (
             <div
               key={index}
-          
-
               className={`border border-gray-300 rounded-md mb-3 overflow-hidden transition-all duration-300 ${
                 isActive ? "bg-blue-50" : "bg-white"
               }`}
