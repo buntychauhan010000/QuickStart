6f1868a0d4a925ca13ff86d221a14cb7cba7751a
EakenderRana <buntychauhan010000@github.com>
2025-07-12 16:23:11 +0530

    new verson

--- src/components/LogoSliders.jsx
+++ src/components/LogoSliders.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/autoplay";
+
 import myob from "../assets/img/myob.png";
 import belimo from "../assets/img/belimo.png";
 import lifegroup from "../assets/img/lifegroup.png";
@@ -6,7 +12,6 @@ import grabyo from "../assets/img/grabyo.png";
 import citrus from "../assets/img/citrus.png";
 import trustly from "../assets/img/trustly.png";
 
-// Logo data moved outside component (performance-friendly)
 const logos = [
   { name: "Myob", src: myob },
   { name: "Belimo", src: belimo },
@@ -19,23 +24,36 @@ const logos = [
 function LogoSliders() {
   return (
     <div className="container mx-auto px-3 py-10 sm:py-20">
-      {/* Logo Grid */}
-      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 gap-10">
+      <Swiper
+        modules={[Autoplay]}
+        autoplay={{ delay: 1000, disableOnInteraction: false }}
+        speed={1000}
+        loop={true}
+        breakpoints={{
+          0: { slidesPerView: 2, spaceBetween: 20 },
+          640: { slidesPerView: 3, spaceBetween: 30 },
+          768: { slidesPerView: 4, spaceBetween: 40 },
+          1024: { slidesPerView: 5, spaceBetween: 50 },
+          1280: { slidesPerView: 6, spaceBetween: 60 },
+        }}
+        className="w-full"
+      >
         {logos.map((item, index) => (
-          <article
-            key={index}
-            data-aos="fade-up"
-            data-aos-delay={index * 200}
-            className="flex justify-center items-center mt-6 md:mt-0 grayscale brightness-110 contrast-75 opacity-60 hover:grayscale-0 hover:brightness-100 hover:contrast-100 hover:opacity-100 transition"
-          >
-            <img
-              src={item.src}
-              alt={`${item.name} logo`}
-              className="h-9 sm:h-12 w-auto"
-            />
-          </article>
+          <SwiperSlide key={index}>
+            <div
+              data-aos="fade-up"
+              data-aos-delay={index * 200}
+              className="flex justify-center items-center grayscale brightness-110 contrast-75 opacity-60 hover:grayscale-0 hover:brightness-100 hover:contrast-100 hover:opacity-100 transition"
+            >
+              <img
+                src={item.src}
+                alt={`${item.name} logo`}
+                className="h-9 sm:h-12 w-auto"
+              />
+            </div>
+          </SwiperSlide>
         ))}
-      </div>
+      </Swiper>
     </div>
   );
 }
